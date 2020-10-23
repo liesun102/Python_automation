cf42c5fd7d4db048c884e7a10af5838d9347c1ab
liesun102 <50430773+liesun102@users.noreply.github.com>
2020-10-23 15:18:57 +0800

    Update ops_settings.py

--- 第一周/06 django template/ops_settings.py
+++ 第一周/06 django template/ops_settings.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.template import Context, loader
-# Create your views here.
 
-def index(request):
-    data = {
-        "name": "rock",
-        "age": 25
-    }
-    data_1 = ["devops", "python"]
-    # return HttpResponse("Hello World !!!")
-    return JsonResponse(data_1, safe=False )
+ALLOWED_HOSTS = ["*"]
 
-def index_template(request):
-    # t = loader.get_template("test.html")
-    # context = {"name": "hello reboot !!!"}
-    # return HttpResponse(t.render(context, request))
-    context = {"name": "reboot"}
-    return render(request, "test.html", context )
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'dashboard.apps.DashboardConfig'
+]
